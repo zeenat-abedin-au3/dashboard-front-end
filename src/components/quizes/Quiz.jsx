f92515bb8930a707fc23dfc54e930f3919f39089
zeenat-abedin-au3 <zeenu.23@gmail.com>
2020-04-11 18:01:39 +0530

    feat: render quizdetails conditionally

--- src/components/quizes/Quiz.jsx
+++ src/components/quizes/Quiz.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import QuizDetails from "./QuizDetails";
 import CreateQuestions from "./CreateQuestions";
 
 const Quiz = () => {
-  const [step, setStep] = useState(1);
+  const {
+    quizDetails: { numQuestion },
+  } = useSelector((state) => state.quiz);
+  const [step, setStep] = useState(0);
+
+  useEffect(() => {
+    setStep(numQuestion);
+  }, [numQuestion]);
 
   if (!step) {
     return <QuizDetails />;
