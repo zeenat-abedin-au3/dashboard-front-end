007b53bbefd5d72d3c480fb5449de3fb92426488
zeenat-abedin-au3 <zeenu.23@gmail.com>
2020-04-12 20:39:28 +0530

    fix: backend link

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const URL =
-  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
+  process.env.NODE_ENV === "development" ? "http://localhost:5000" : "https://mytutor-dashboard-api.herokuapp.com";
 
 export default axios.create({
   baseURL: URL,
