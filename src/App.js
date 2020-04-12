6b48c5180f9498153f3c23030a544feb21bf371f
zeenat-abedin-au3 <zeenu.23@gmail.com>
2020-04-12 19:14:38 +0530

    feat: handle home route

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { useEffect } from "react";
-import { Switch, Route } from "react-router-dom";
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 import NavBar from "./components/headers/NavBar";
-import Home from "./components/home/Home";
 import Auth from "./components/auth/Auth";
 import Dashboard from "./components/dashboard/Dashboard";
 import Quizes from "./components/dashboard/Quizes";
 
 const App = () => {
+  const { token } = useSelector((state) => state.auth);
+  const localToken = localStorage.getItem("token");
   return (
     <>
       <NavBar />
       <Switch>
-        <Route path="/" exact component={Home} />
         <Route path="/login" exact component={Auth} />
         <Route path="/dashboard" exact component={Dashboard} />
         <Route path="/quizes" exact component={Quizes} />
+        <Redirect to={`${localToken || token ? "/dashboard" : "/login"}`} />
       </Switch>
     </>
   );
